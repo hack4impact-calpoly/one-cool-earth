bad151895ae31cf0add89a9476bbb38e8214927e
James R <Jamesliamr@outlook.com>
2021-01-24 13:48:48 -0800

    Started setting up middleware

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,9 @@
 const express = require('express')
 const app = express();
+const login = require('./api/login.js');
+const sign_up = require('./api/sign-up.js');
+
+app.use(express.json())
+
+app.use('api/login', login);
+app.use('api/sign-up', sign_up);
