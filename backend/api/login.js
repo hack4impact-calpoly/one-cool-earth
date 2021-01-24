7cfb9a05261abf80067045ccd4dac16b294846cd
ghuarng <gary0928@gmail.com>
2021-01-23 18:52:09 -0800

    Add schema support, comments

--- backend/api/login.js
+++ backend/api/login.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+//User Schema -- filename could be different
+const User = require('./model/user');
+
+/*
+	POST user's email and password to login endpoint
+*/
 app.post('/api/login', async(req, res) => {
-	res.status(200);
+	//Pass in user email
+	email = User.find({title:email});
+	if(typeof email === undefined || email.length === 0){
+		res.send('ERROR: NO EMAIL FOUND');
+	}
 	
+	//Pass in user password
+	pass = User.find({title:password});
+	if(typeof pass === undefined || pass.length === 0){
+		res.send('ERROR: NO PASSWORD FOUND');
+	}
+	
+	res.status(200);
 	//Placeholder -- will replace with actual POST request
-	//Need to know USER SCHEMA
 	res.send('POST user email and password');
 })
 
