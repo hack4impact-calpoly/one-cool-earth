6b149bbf8ecb4a0d1bd26a362676a15ea97b9ffc
ghuarng <gary0928@gmail.com>
2021-01-23 18:29:13 -0800

    Add placeholder POST request

---
+++ backend/api/login.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+app.post('/api/login', async(req, res) => {
+	res.status(200);
+	
+	//Placeholder -- will replace with actual POST request
+	//Need to know USER SCHEMA
+	res.send('POST user email and password');
+})
+
+
