bdfda66bc729cdff73a2bc0070725be29dca5535
konanyuta <58442094+konanyuta@users.noreply.github.com>
2021-03-06 11:31:45 -0800

    added logic to check if email exists or not

--- backend/api/signup.js
+++ backend/api/signup.js
@@ -5,12 +5,16 @@ const router = express.Router();
 
 router.post('/', asynch (req, res) => {
    res.status(200);
-   
-   const user = await User.updateOne({email: req.body.email}, 
+
+   if (Recipe.findOne({'email': req.body.email})) {
+      const user = await User.updateOne({email: req.body.email}, 
                                        {admin: req.body.email, name: req.body.name, password: req.body.password, dates: req.body.dates, location: req.body.location, preferences: req.body.preferences},
                                        {upsert: true});
-   res.send('Welcome ' + req.body.name + '!\n Your account has been successfully created');
-
+      res.send('Welcome ' + req.body.name + '!\n Your account has been successfully created');
+   }
+   else {
+   	res.send('Your email (' + req.body.email + ') is alriady associated with an account, please log in.');
+   }
 })
 
 module.exports = router;
