dd3c9a510da6f99f3de06b61b0945f47114608c2
konanyuta <58442094+konanyuta@users.noreply.github.com>
2021-03-09 14:25:01 -0800

    removed the check for admin

--- backend/api/signup.js
+++ backend/api/signup.js
@@ -8,7 +8,7 @@ router.post('/', asynch (req, res) => {
 
    if (Recipe.findOne({'email': req.body.email})) {
       const user = await User.updateOne({email: req.body.email}, 
-                                       {admin: req.body.email, name: req.body.name, password: req.body.password, dates: req.body.dates, location: req.body.location, preferences: req.body.preferences},
+                                       {name: req.body.name, password: req.body.password, dates: req.body.dates, location: req.body.location, preferences: req.body.preferences},
                                        {upsert: true});
       res.send('Welcome ' + req.body.name + '!\n Your account has been successfully created');
    }
