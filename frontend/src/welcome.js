f105cb6c2bf6725ff8e565a01b4eca3bc534d179
ghuarng <gary0928@gmail.com>
2021-03-10 13:07:30 -0800

    Move HTML to JS file

--- frontend/src/welcome.js
+++ frontend/src/welcome.js
@@ -1,14 +1,13 @@
-<!DOCTYPE html>
-<html lang="en US">
-    <head>
-        <link rel="stylesheet" href="./css/welcome.css">
-        <title>Welcome!</title>
-    </head>
+import React from "react"
+import "./css/welcome.css"
 
+class Welcome extends React.Component {
+    render(){
+    return(
     <body>
         <header>
             <div class='returnHome'>
-                <img src="./logo.svg" height="50" alt="Logo">
+                <img src="./logo.svg" height="50" alt="Logo"/>
                 <button>RETURN HOME</button>
             </div>
 
@@ -20,14 +19,14 @@
 
         <div class='banner'>
             <p1>[BANNER]</p1>
-            <br>
+            <br/>
         </div>
 
         <main>
             <div class='planning'>
                 <div class='engagements'>
                     <p1>Current Engagements</p1>
-                    <br>
+                    <br/>
                     <button>EDIT</button>
                 </div>
 
@@ -38,12 +37,13 @@
 
             <div class='news'>
                 <p1>Organization News / Highlighted Event</p1>
-                <br>
-                <img src="./logo.svg" height="200" alt="Highlight">
+                <br/>
+                <img src="./logo.svg" height="200" alt="Highlight"/>
             </div>
         </main>
         
     </body>
+    );}
+}
 
-</html>
-
+export default Welcome;
