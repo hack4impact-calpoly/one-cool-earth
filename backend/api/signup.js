8520ad101b24c2cf1a9c7e83b032ebb9561fb1ef
sleepyStick <itsirisho@gmail.com>
2021-04-05 01:26:26 -0700

    Update signup.js

--- backend/api/signup.js
+++ backend/api/signup.js
@@ -8,8 +8,8 @@ router.post('/', asynch (req, res) => {
 
    if (Recipe.findOne({'email': req.body.email})) {
       const user = await User.updateOne({email: req.body.email}, 
-                                       {name: req.body.name, password: req.body.password, dates: req.body.dates, location: req.body.location, preferences: req.body.preferences},
-                                       {upsert: true});
+                                        {name: req.body.name, password: req.body.password, dates: req.body.dates, location: req.body.location, preferences: req.body.preferences},
+                                        {upsert: true});
       res.send('Welcome ' + req.body.name + '!\n Your account has been successfully created');
    }
    else {
