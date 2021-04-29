1bd42cb7c3ab5334a8661804ae0736df7cc1d01d
James R <Jamesliamr@outlook.com>
2021-04-29 15:21:23 -0700

    Cleaned up my App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,40 @@
 import './css/App.css';
+import Header from './Header';
 import Admin from './admin.js';
 import LandingPage from './LandingPage';
 import Signup from './signup.js';
 import Login from './login.js';
+// import Spreadsheets from './spreadsheets.js';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import Spreadsheets from './spreadsheets.js';
 
 function App() {
   return (
-    <Spreadsheets />
-      
+    <div>
+    <Header></Header>
+    <BrowserRouter>
 
-    // <BrowserRouter>
-    //   <div className="App">
-    //     <Switch>
-    //       <Route exact path="/spreadsheets.js">
-	  //   <LandingPage />
-    //       </Route>
-	  // <Route path="/login">
-	  //   <Login />
-	  // </Route>
-	  // <Route path="/signup">
-	  //   <Signup />
-	  // </Route>
-	  // <Route path="/admin">
-	  //   <Admin />
-	  // </Route>
-    //     </Switch>
-    //   </div>
-    // </BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+	    <LandingPage />
+          </Route>
+	  <Route path="/login">
+	    <Login />
+	  </Route>
+    {/* <Route path="/spreadsheets">
+      <Spreadsheets />
+	  </Route> */}
+	  <Route path="/signup">
+	    <Signup />
+	  </Route>
+	  <Route path="/admin">
+	    <Admin />
+	  </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+    </div>
   );
 }
-
 
 export default App;
