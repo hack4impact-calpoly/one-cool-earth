8fe893de2690ca9d4f94bddbdba30f51b7624e2f
ksittner <kevinsittner6@gmail.com>
2021-05-03 02:55:16 -0700

    Tested display of API data and adjusted UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Admin from './admin.js';
 import LandingPage from './LandingPage';
 import Signup from './signup.js';
 import Login from './login.js';
+import Welcome from './welcome.js';
 // import Spreadsheets from './spreadsheets.js';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
@@ -11,27 +12,30 @@ function App() {
   return (
     <div>
     <Header></Header>
-    <BrowserRouter>
-
-      <div className="App">
-        <Switch>
-          <Route exact path="/">
-	    <LandingPage />
-          </Route>
-	  <Route path="/login">
-	    <Login />
-    {/* <Route path="/spreadsheets">
-      <Spreadsheets />
-	  </Route> */}
-	  <Route path="/signup">
-	    <Signup />
-	  </Route>
-	  <Route path="/admin">
-	    <Admin />
-	  </Route>
-        </Switch>
-      </div>
-    </BrowserRouter>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route exact path="/">
+	            <LandingPage />
+            </Route>
+	          <Route path="/login">
+	            <Login />
+            </Route>
+            {/* <Route path="/spreadsheets">
+              <Spreadsheets />
+            </Route> */}
+	          <Route path="/signup">
+	            <Signup />
+	          </Route>
+	          <Route path="/admin">
+	            <Admin />
+	          </Route>
+            <Route path='/welcome'>
+              <Welcome />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
