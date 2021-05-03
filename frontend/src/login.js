da899bb68398fa01ac8e180b98b8b5ec5ae8af57
gary <gary0928@gmail.com>
2021-05-02 20:28:45 -0700

    Change from functional to class component

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,27 +1,45 @@
+import React from 'react';
 import './css/login.css';
 
-function Login() {
-  return (
-    <body>
-      {/* <div className = 'box1'>
-        <button>Return home</button>
-      </div> */}
-      <div className = 'box2'>
-        <p>Log In</p>
-      </div>
-      <div className = 'box3'>
-        <div id = 'email-div'>
-          <label for='email'>EMAIL</label>
-          <input id = 'email'></input>
-        </div>
-        <div id = 'password-div'>
-          <label for = 'password'>PASSWORD</label>
-          <input id = 'password' type = 'password'></input>
-        </div>
-        <button id = 'log-in-button'> Log In</button>
-      </div>
-    </body>
-  );
-}
+// function postLoginData(){
+//   const inputEmail = document.getElementById("email").value;
+//   const inputPassword = document.getElementById("password").value;
 
+//   if(inputEmail == null || inputPassword == null){
+//     return;
+//   }
+
+//   const loginData={
+//     email: inputEmail,
+//     password: inputPassword
+//   }
+  
+//   console.log(loginData);
+// }
+
+class Login extends React.Component{
+  render(){
+    return (
+      <body>
+        {/* <div className = 'box1'>
+          <button>Return home</button>
+        </div> */}
+        <div className = 'box2'>
+          <p>Log In</p>
+        </div>
+        <div className = 'box3'>
+          <div id = 'email-div'>
+            <label for='email'>EMAIL</label>
+            <input id = 'email'></input>
+          </div>
+          <div id = 'password-div'>
+            <label for = 'password'>PASSWORD</label>
+            <input id = 'password' type = 'password'></input>
+          </div>
+          <button id = 'log-in-button'> Log In</button>
+        </div>
+      </body>
+    );
+  }
+}
 export default Login;
