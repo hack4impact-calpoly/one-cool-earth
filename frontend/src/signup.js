9e522c83e998c9f1e456ba1e3f71bf6df8a29ec5
sophsuan <47379998+sophsuan@users.noreply.github.com>
2021-05-05 14:04:05 -0700

    got user input and console logged as json object

--- frontend/src/signup.js
+++ frontend/src/signup.js
@@ -4,14 +4,67 @@ import 'react-dropdown/style.css';
 import './css/signup.css';
 
 const volunteerOptions = [
-    'Garden Workday Volunteer', 'Special Events Volunteer', 'Garden Educator Assistant', 
-    'Office/Remote Volunteer', 'Unsure or Interested in Multiple Opportunities' 
+    'Garden Workday Volunteer', 'Special Events Volunteer', 'Garden Educator Assistant', 'Office/Remote Volunteer', 'Unsure or Interested in Multiple Opportunities'
   ]
   const locationOptions = [
     'South County', 'Coastal', 'San Luis Obispo', 'North County'
   ]
 
+  const selected = [  ]
+
+  
 class Signup extends React.Component {
+
+    constructor (props) {
+        super(props)
+        this.state = {
+          selected: ''
+        }
+        console.log('new')
+        console.log(this)
+        this._onSelect = this._onSelect.bind(this)
+      }
+
+      _onSelect (option, id) {
+          selected.push(option.label)
+      }
+
+
+    postSignUpData(){
+        const first = document.getElementById("first-name").value;
+        const last = document.getElementById("last-name").value;
+        const preferences = selected[0];
+        const phone = document.getElementById("phone-number").value;
+        const email = document.getElementById("email").value;
+        const location = selected[1];
+
+      //Don't submit data unless both fields are non-empty
+      if(first === "" || last === "" || preferences === "" || phone === "" || email === "" || location === ""){
+        return;
+      }
+
+      const SignUpData={
+        firstName: first,
+        lastName: last,
+        preferences: preferences,
+        phoneNum : phone,
+        email : email,
+        loc : location
+      }
+
+      console.log(SignUpData);
+    }
+
+    clear() {
+        document.getElementById("first-name").value = '';
+        document.getElementById("last-name").value = '';
+        vol_val = "";
+        document.getElementById("phone-number").value = '' ;
+        document.getElementById("email").value = '';
+        loc_val = "";
+    }
+
+
     render() {
         return (
             <div className = 'wrapper'>
@@ -30,7 +83,8 @@ class Signup extends React.Component {
                         </div>
                         <div id ='volunteer-preferences-field' className='drop-down'>
                             <label for='volunteer-preferences'>Volunteer Preferences</label>
-                            <Dropdown id = 'volunteer-preferences' options={volunteerOptions} placeholder='Select'/>
+                            <Dropdown id = 'volunteer-preferences' onChange={this._onSelect} options={volunteerOptions} placeholder='Select'/>
+
                         </div> 
                     </div>
                     <div className='fields-column'>
@@ -44,10 +98,12 @@ class Signup extends React.Component {
                         </div>
                         <div id='location-preference-field' className='drop-down'>
                             <label for='location-preference'>Location Preference</label>
-                            <Dropdown id = 'location-preference' options={locationOptions} placeholder='Select'/>
+                            <Dropdown id = 'location-preference' onChange={this._onSelect} options={locationOptions} placeholder='Select'/>
                         </div>
                     </div>
                 </div>
+
+                <button id = 'sign-up-button' onClick={() => {this.postSignUpData(); this.clear()}}> Sign Up</button>
             </div>
     );
   }
