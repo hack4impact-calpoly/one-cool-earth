5c4c2ef59feb16aa489c0f1a55dfbfe189cbea09
Colem18 <cole2018@gmail.com>
2021-05-07 00:56:02 -0700

    switched calendarPage from function ____ to class ____

--- frontend/src/calendar.js
+++ frontend/src/calendar.js
@@ -1,33 +1,36 @@
 import './css/calendar.css';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 
 
 
-function CalendarPage() {
-  return (
-    <body>
-      <div className = 'header'>
-        <div className= 'buttons'>
-          <button id = 'home-button'>Home</button>
-          <button id = 'logout-button'>Log out</button>
+class CalendarPage extends React.Component{
+  render() {
+    return (
+      <body>
+        <div className = 'header'>
+          <div className= 'buttons'>
+            <button id = 'home-button'>Home</button>
+            <button id = 'logout-button'>Log out</button>
+          </div>
+          <div className = 'text'>
+            <p id = 'main-text'> Calendar</p>
+          </div>
         </div>
-        <div className = 'text'>
-          <p id = 'main-text'> Calendar</p>
+        <div className = 'calendar'>
+          <FullCalendar
+            className = 'fc'
+            plugins={[ dayGridPlugin ]} 
+            initialView = 'dayGridMonth'
+            height = 'auto'
+            width = 'auto'
+          />
         </div>
-      </div>
-      <div className = 'calendar'>
-        <FullCalendar
-          className = 'fc'
-          plugins={[ dayGridPlugin ]} 
-          initialView = 'dayGridMonth'
-          height = 'auto'
-          width = 'auto'
-        />
-      </div>
-    </body>
-  );
+      </body>
+    );
+  }
 }
 
 export default CalendarPage;
