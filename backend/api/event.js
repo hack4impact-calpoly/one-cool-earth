2f539296ac3da2603b8cfcd715efe92c3397124d
James R <Jamesliamr@outlook.com>
2021-05-13 15:43:31 -0700

    Finished and tested endppoint

--- backend/api/event.js
+++ backend/api/event.js
@@ -3,30 +3,28 @@ const express = require('express');
 const Event = require('../models/Event');
 const router = express.Router();
 
-const editEvent = async (name, date, location, volunteerPreferences) => {
-    Event.findOne({"name": name}).then(function(event) {
-        if (name) {
-           event.name = name.first
+const createEvent = async (eventName, eventDate, eventLocation, eventVolunteerPreferences) => {
+    Event.findOne({name: eventName}).then((data) => {
+        if(!data)
+        {
+            const event = Event.create(
+            {
+                name: eventName, 
+                date: eventDate, 
+                location: eventLocation, 
+                volunteerPreferences: 
+                eventVolunteerPreferences
+            });
         }
-        if (date) {
-           event.date = date
-        }
-        if (location) {
-           event.location = location
-        }
-        if (volunteerPreferences) {
-           event.volunteerPreferences = volunteerPreferences
-        }
-        event.save()
-      });
+    });
 };
 
 router.post('/create', async (req, res) => {
-    name = req.body.name
-    date = req.body.date
-    location = req.body.location
-    volunteerPreferences = req.body.volunteerPreferences
-    await editEvent(name, date, location, volunteerPreferences)
+    const eventName = req.body.name
+    const date = req.body.date
+    const location = req.body.location
+    const volunteerPreferences = req.body.volunteerPreferences
+    await createEvent(eventName, date, location, volunteerPreferences)
     res.send("success")
  });
  
