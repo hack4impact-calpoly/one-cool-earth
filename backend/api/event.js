c0e993e8167bdf8270a4191d02868a0b414c0204
James R <Jamesliamr@outlook.com>
2021-05-14 16:54:01 -0700

    FIrst attempt at creating delete event endpoint

--- backend/api/event.js
+++ backend/api/event.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const Event = require('../models/Event');
+//const User = require('../models/User');
 const router = express.Router();
 
 const createEvent = async (eventName, eventDate, eventLocation, eventVolunteerPreferences) => {
@@ -26,6 +27,36 @@ router.post('/create', async (req, res) => {
     const volunteerPreferences = req.body.volunteerPreferences
     await createEvent(eventName, date, location, volunteerPreferences)
     res.send("success")
+ });
+
+ const deleteEvent = async (eventName, eventDate, eventLocation, eventVolunteerPreferences) => {
+    Event.findOne({name: eventName}).then((data) => {
+        if(data)
+        {
+            Event.deleteOne({name: eventName})
+        }
+        else
+        {
+            res.send("Couldn't find event")
+        }
+    });
+};
+
+
+
+ router.post('/editEvent', async (req, res) => {
+    if (req.user.admin)
+    {
+        const eventName = req.body.name
+        const date = req.body.date
+        const location = req.body.location
+        const volunteerPreferences = req.body.volunteerPreferences
+        await deleteEvent(eventName);
+        res.send("success")
+    }
+    else {
+        res.send("You must be an admin to delete event")
+     }
  });
  
  module.exports = router;
