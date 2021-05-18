da75cac2e3ae5674fe558bdfa7b247b8828b92eb
Archie Jones <jonesarchie37@gmail.com>
2021-05-18 15:41:33 -0700

    returns all events

--- backend/index.js
+++ backend/index.js
@@ -25,12 +25,14 @@ const signUpEndpoint = require('./api/signup.js')
 const authEndpoint = require('./api/auth')
 const adminEndpoint = require('./api/admin')
 const userEndpoint = require('./api/user')
+const eventEndpoint = require('./api/event')
 
 app.use('api/login', loginEndpoint)
 app.use('api/signup', signUpEndpoint)
 app.use('/api/auth', authEndpoint)
 app.use('/api/admin', adminEndpoint)
 app.use('/api/user', userEndpoint)
+app.use('/api/event', eventEndpoint)
 
 app.get('/api/user', async (req, res) => {
     res.redirect(`${process.env.SERVER_URL}/api/auth`)
