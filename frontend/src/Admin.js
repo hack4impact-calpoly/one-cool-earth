523bc547cba3d3ec5538ad3f6bae3d9f93d01211
Colem18 <cole2018@gmail.com>
2021-05-18 23:21:40 -0700

    Added links to the two existing admin buttons

--- frontend/src/Admin.js
+++ frontend/src/Admin.js
@@ -3,25 +3,25 @@ import {Button, Col, Row} from 'react-bootstrap';
 import './css/Admin.css';
 
 class Admin extends React.Component {
-	render() {
-		return (
-			<div>
-				<body>
-					<div id="main">
-						<div id="createEventBox">
-							<Button href={process.env.REACT_APP_SERVER_URL + "/create-event"}>CREATE EVENT</Button>
-						</div>
-						<div id="viewVolunteerDataBox">
-							<Button>VIEW VOLUNTEER DATA</Button>
-						</div>
-						<div id="editAnnouncementsBox">
-							<Button>EDIT ANNOUNCEMENTS</Button>
-						</div>
-					</div>
-				</body>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <body>
+	  <div id="main">
+	    <div id="createEventBox">
+	      <Button href="/create-event">CREATE EVENT</Button>
+	    </div>
+	    <div id="viewVolunteerDataBox">
+	      <Button href="/spreadsheets">VIEW VOLUNTEER DATA</Button>
+	    </div>
+	    <div id="editAnnouncementsBox">
+	      <Button>EDIT ANNOUNCEMENTS</Button>
+	    </div>
+	  </div>
+	</body>
+      </div>
+    );
+  }
 }
 
 export default Admin;
