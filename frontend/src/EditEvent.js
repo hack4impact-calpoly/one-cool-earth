68e9d0eb93df605df71a31b5262a87610928831b
sophsuan <47379998+sophsuan@users.noreply.github.com>
2021-05-23 20:42:06 -0700

    dropdown select multiple createEvent, signUp, edit-user, edit-event

--- frontend/src/EditEvent.js
+++ frontend/src/EditEvent.js
@@ -1,34 +1,59 @@
-import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
 import React from "react";
 import "./css/CreateEvent.css";
 import { Button } from "react-bootstrap";
+import Select from "react-select";
 
-// options from backend
+// DUMMY: need to replace with backend data
 const volunteerOptions = [
-  "Garden Workday Volunteer",
-  "Special Events Volunteer",
-  "Garden Educator Assistant",
-  "Office/Remote Volunteer",
-  "Unsure or Interested in Multiple Opportunities",
+  { value: "garden workday", label: "Garden Workday Volunteer" },
+  { value: "special events", label: "Special Events Volunteer" },
+  { value: "garden educator", label: "Garden Educator Assistant" },
+  { value: "office/remote", label: "Office/Remote Volunteer" },
+  { value: "unsure", label: "Unsure or Interested in Multiple Opportunities" },
 ];
-
-// options from backend
 const locationOptions = [
-  "South County",
-  "Coastal",
-  "San Luis Obispo",
-  "North County",
+  { value: "south county", label: "South County" },
+  { value: "coastal", label: "Coastal" },
+  { value: "san luis", label: "San Luis Obispo" },
+  { value: "north county", label: "North County" },
 ];
 
 // dummy data: change to data stored from backend
 const stored_event_name = "Garden Day";
 const stored_date = "01-02-03";
-const stored_volunteer_preference = "Garden Workday Volunteer";
-const stored_location = "Coastal";
+const stored_volunteer_preference = [
+  { value: "garden workday", label: "Garden Workday Volunteer" },
+  { value: "special events", label: "Special Events Volunteer" },
+  { value: "unsure", label: "Unsure or Interested in Multiple Opportunities" },
+];
+const stored_location = [
+  { value: "south county", label: "South County" },
+  { value: "north county", label: "North County" },
+];
 
 class EditEvent extends React.Component {
+  state = {
+    volSelected: stored_volunteer_preference,
+    locSelected: stored_location,
+  };
+
+  handleVolChange = (volSelected) => {
+    this.setState({ volSelected }, () =>
+      console.log(`Option selected:`, this.state.volSelected)
+    );
+  };
+
+  handleLocChange = (locSelected) => {
+    this.setState({ locSelected }, () =>
+      console.log(`Option selected:`, this.state.locSelected)
+    );
+  };
+
   render() {
+    const { volSelected } = this.state;
+    const { locSelected } = this.state;
+
     return (
       <body id="events-body">
         <div className="box2">
@@ -44,18 +69,44 @@ class EditEvent extends React.Component {
         <div className="box3">
           <div id="volunteer-preferences-field" className="drop-down">
             <label for="volunteer-preferences">Volunteer Preferences</label>
-            <Dropdown
-              id="volunteer-preferences"
+            <Select
+              value={volSelected}
+              onChange={this.handleVolChange}
               options={volunteerOptions}
-              placeholder={stored_volunteer_preference}
+              isMulti={true}
+              theme={(theme) => ({
+                ...theme,
+                borderRadius: 0,
+                borderColor: "black",
+                colors: {
+                  ...theme.colors,
+                  neutral20: "black", // this is border line color
+                },
+                spacing: {
+                  baseUnit: 8,
+                },
+              })}
             />
           </div>
           <div id="location-preference-field" className="drop-down">
             <label for="location-preference">Location Preference</label>
-            <Dropdown
-              id="location-preference"
+            <Select
+              value={locSelected}
+              onChange={this.handleLocChange}
               options={locationOptions}
-              placeholder={stored_location}
+              isMulti={true}
+              theme={(theme) => ({
+                ...theme,
+                borderRadius: 0,
+                borderColor: "black",
+                colors: {
+                  ...theme.colors,
+                  neutral20: "black", // this is border line color
+                },
+                spacing: {
+                  baseUnit: 8,
+                },
+              })}
             />
           </div>
         </div>
