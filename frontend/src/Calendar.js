e0d76f2fa287485ccb51588f03f4f4fdf1b7f18f
Colem18 <cole2018@gmail.com>
2021-05-24 18:58:45 -0700

    modal stuff

--- frontend/src/Calendar.js
+++ frontend/src/Calendar.js
@@ -1,33 +1,65 @@
 import './css/Calendar.css';
-import React from 'react';
+import React, {useState} from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import {Container} from "react-bootstrap";
+import {Container, Modal, Button} from "react-bootstrap";
 import './css/Calendar.css';
+import interactionPlugin from "@fullcalendar/interaction";
 
 class CalendarPage extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      modalVisible: false,
+      dateClicked: ""
+    }
+  }
   render (){
     return (
       <Container>
-        <body>
-          <div className = 'header'>
-            <div className = 'text'>
-              <p id = 'main-text'> Calendar</p>
-            </div>
+        <div className = 'header'>
+          <div className = 'text'>
+            <p id = 'main-text'> Calendar</p>
           </div>
-          <div className = 'calendar'>
-            <FullCalendar
-              className = 'fc'
-              plugins={[ dayGridPlugin ]}
-              initialView = 'dayGridMonth'
-              height = 'auto'
-              width = 'auto'
-            />
-          </div>
-        </body>
+        </div>
+        <div className = 'calendar'>
+          <FullCalendar
+            className = 'fc'
+            plugins={[ dayGridPlugin, interactionPlugin ]}
+            initialView = 'dayGridMonth'
+	    dateClick= {function(arg){console.log(arg.dateStr)}}
+            height = 'auto'
+            width = 'auto'
+          />
+        </div>
+	<Modal show={this.state.modalVisible} onHide={this.hideModal}>
+          <Modal.Header>
+            <Modal.Title>Events on {this.state.dateClicked}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+	    This is the modal body
+	  </Modal.Body>
+          <Modal.Footer>
+	    <Button onClick={this.hideModal}>Close</Button>
+	  </Modal.Footer>
+        </Modal>
       </Container>
     );
   }
-}
+  
+  showModal = () => {
+    this.setState({modalVisible: true});
+  }
+
+  hideModal = () => {
+    this.setState({modalVisible: false});
+  }
+  
+  handleDateClick = (arg) =>{
+    this.setState({dateClicked: `${arg.date.getMonth()}/${arg.date.getDate()}/${arg.date.getFullYear()}`});
+    console.log(arg.dateStr);
+    this.showModal();
+  }
+} 
 
 export default CalendarPage;
