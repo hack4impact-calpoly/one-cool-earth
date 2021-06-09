ce540a67c35379d078ba42e6c98025fbba785df5
Colem18 <cole2018@gmail.com>
2021-06-08 23:40:56 -0700

    Implemented the modal displaying event dropdowns

--- frontend/src/Calendar.js
+++ frontend/src/Calendar.js
@@ -4,19 +4,63 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import {Container, Modal, Button} from "react-bootstrap";
 import interactionPlugin from "@fullcalendar/interaction";
-import DateModal from "./DateModal.js"
+import DateModal from "./DateModal.js";
+
+const sampleEvents = [
+  {
+    id: 1,
+    name: "Event 1",
+    startTime: (new Date()).getTime(),
+    endTime: (new Date()).getTime(),
+    location: "Paso Robles",
+    description: "This is the decription for Event 1"
+  },
+  {
+    id: 2,
+    name: "Event 2",
+    startTime: (new Date()).getTime(),
+    endTime: (new Date()).getTime(),
+    location: "Arroyo Grande",
+    description: "This is the decription for Event 2. This has a lot of lines to check the wrap around of the modal."
+  },
+  {
+    id: 3,
+    name: "Event 3",
+    startTime: (new Date()).getTime(),
+    endTime: (new Date()).getTime(),
+    location: "San Luis Obispo",
+    description: "This is the decription for Event 3"
+  }
+]
 
 class CalendarPage extends React.Component{
   constructor(){
     super();
     this.state={
       dateModalVisible: false,
-      dateData: {
-                  dateStr: "",
-	          events: {}
-                }
+      dateStr: "",
+      events: []
     }
   }
+
+  showDateModal = () => {
+    this.setState({dateModalVisible: true});
+  }
+
+  hideDateModal = () => {
+    this.setState({dateModalVisible: false});
+  }
+
+  handleDateClick = (arg) =>{
+    this.setState({
+      dateStr: `${arg.date.getMonth()+1}/${arg.date.getDate()}/${arg.date.getFullYear()}`,
+      events: sampleEvents
+      });
+    console.log(this.state.dateStr);
+    console.log(this.state.events);
+    this.showDateModal();
+  }
+
   render (){
     return (
       <Container>
@@ -38,23 +82,11 @@ class CalendarPage extends React.Component{
 	<DateModal 
 	    show={this.state.dateModalVisible}
 	    onHide={this.hideDateModal}
-	    dateData={this.state.dateData}
+	    dateStr={this.state.dateStr}
+	    events={this.state.events}
 	></DateModal>
       </Container>
     );
-  }
-  
-  showDateModal = () => {
-    this.setState({dateModalVisible: true});
-  }
-
-  hideDateModal = () => {
-    this.setState({dateModalVisible: false});
-  }
-  
-  handleDateClick = (arg) =>{
-    this.setState({dateData: {dateStr: `${arg.date.getMonth()+1}/${arg.date.getDate()}/${arg.date.getFullYear()}`}});
-    this.showDateModal();
   }
 } 
 
--- frontend/src/DateModal.js
+++ frontend/src/DateModal.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import './css/DateModal.css';
 import {Button, Modal} from 'react-bootstrap';
+import Collapsible from 'react-collapsible';
 
 class DateModal extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      modalVisible: false,
+      modalVisible: false
     }
+  }
+
+  getEventComponents(){
+	  return (this.props.events.map((e) =>
+	    <Collapsible trigger={e.name}>
+	      {
+	        Object.keys(e).map(function(eventAttribute){
+                  return <h6>{eventAttribute + ": " + e[eventAttribute]}</h6>
+		})
+	      }
+              <div class="signUpBox"><Button class="signUpButton">Sign up</Button></div>
+            </Collapsible>));
   }
 
   render() {
@@ -14,10 +28,10 @@ class DateModal extends React.Component {
       <>
 	<Modal show={this.props.show} onHide={this.props.onHide}>
           <Modal.Header>
-            <Modal.Title>Events on {this.props.dateData.dateStr}</Modal.Title>
+            <Modal.Title>Events on {this.props.dateStr}</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            This is the new modal body
+	    {this.getEventComponents()}
           </Modal.Body>
           <Modal.Footer>
             <Button onClick={this.props.onHide}>Close</Button>
