5935331f54f72a01c3ffd0ff87b147bd561651ae
Quentin Monasterial <quinmona98@gmail.com>
2021-06-11 00:20:33 -0700

    added more calendar features

--- frontend/src/Calendar.js
+++ frontend/src/Calendar.js
@@ -10,6 +10,7 @@ const sampleEvents = [
   {
     id: 1,
     name: "Event 1",
+    date: new Date("Wed Jun 02 2021"),
     startTime: (new Date("Wed Jun 02 2021 03:30:16 GMT-0700")),
     endTime: (new Date("Wed Jun 02 2021 05:30:16 GMT-0700")),
     location: "Paso Robles",
@@ -18,79 +19,145 @@ const sampleEvents = [
   {
     id: 2,
     name: "Event 2",
-    startTime: (new Date("Fri Jun 04 2021 09:30:16 GMT-0700")),
-    endTime: (new Date("Fri Jun 04 2021 10:30:16 GMT-0700")),
+    date: new Date("Wed Jun 10 2021"),
+    startTime: (new Date("Fri Jun 10 2021 09:30:16 GMT-0700")),
+    endTime: (new Date("Fri Jun 10 2021 10:30:16 GMT-0700")),
     location: "Arroyo Grande",
     description: "This is the decription for Event 2. This has a lot of lines to check the wrap around of the modal."
   },
   {
     id: 3,
     name: "Event 3",
-    startTime: (new Date("Mon Jun 07 2021 13:30:16 GMT-0700")),
-    endTime: (new Date("Mon Jun 07 2021 14:30:16 GMT-0700")),
+    date: new Date("Wed Jun 23 2021"),
+    startTime: (new Date("Mon Jun 23 2021 13:30:16 GMT-0700")),
+    endTime: (new Date("Mon Jun 23 2021 14:30:16 GMT-0700")),
+    location: "San Luis Obispo",
+    description: "This is the decription for Event 3"
+  },
+  {
+    id: 4,
+    name: "Event 4",
+    date: new Date("Wed Jun 23 2021"),
+    startTime: (new Date("Mon Jun 23 2021 13:30:16 GMT-0700")),
+    endTime: (new Date("Mon Jun 23 2021 14:30:16 GMT-0700")),
     location: "San Luis Obispo",
     description: "This is the decription for Event 3"
   }
 ]
 
 class CalendarPage extends React.Component{
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state={
-      dateModalVisible: false,
-      events: sampleEvents,
+      showModal: false,
+      events: [],
       dateClickedStr: "",
-      dateClickedEvents: []
+      dateClickedEvents: [],
+      eventClicked: null
     }
   }
 
-  componentDidMount() {
-    //$('#calendar').fullCalendar('addEventSource', this.state.events);
+  componentDidMount = () => {
+    this.setState({ events: sampleEvents})
+  }
+
+  getEvents = (date) => {
+    let events = []
+    for (let event of sampleEvents) {
+      if(event.date.getDate() === date.getDate() &&
+          event.date.getMonth() === date.getMonth() &&
+          event.date.getFullYear() === date.getFullYear())
+        events.push(event)
+    }
+    return events
   }
 
   showDateModal = () => {
-    this.setState({dateModalVisible: true});
+    this.setState({showModal: true});
   }
 
   hideDateModal = () => {
-    this.setState({dateModalVisible: false});
+    this.setState({
+      showModal: false,
+      dateClickedStr: "",
+      dateClickedEvents: [],
+      eventClicked: ""
+    });
   }
 
-  handleDateClick = (arg) =>{
+  handleDateClick = (arg) => {
+    const events = this.getEvents(arg.date)
     this.setState({
       dateClickedStr: `${arg.date.getMonth()+1}/${arg.date.getDate()}/${arg.date.getFullYear()}`,
-      dateClickedEvents: sampleEvents
-      });
-    this.showDateModal();
+      dateClickedEvents: events
+    });
+    this.showDateModal()
   }
 
-  render (){
+  handleEventClick = (arg) => {
+    let eventClicked = null, events = []
+
+    for (let event of this.state.events) {
+      if(event.name === arg.event.title) {
+        eventClicked = event
+        break
+      }
+    }
+
+    events = this.getEvents(eventClicked.date)
+
+    console.log(events)
+    this.setState({
+      dateClickedStr: `${eventClicked.date.getMonth()+1}/${eventClicked.date.getDate()}/${eventClicked.date.getFullYear()}`,
+      dateClickedEvents: events,
+      eventClicked: arg.event.title
+    })
+    this.showDateModal()
+
+  }
+
+  render () {
     return (
-      <Container>
-        <div className = 'header'>
-          <div className = 'text'>
-            <p id = 'main-text'> Calendar</p>
-          </div>
-        </div>
-        <div className = 'calendar'>
+      <div id='calendar-page'>
+        <div id="calendar-container">
           <FullCalendar
-            className = 'fc'
             plugins={[ dayGridPlugin, interactionPlugin ]}
+            headerToolbar={{
+              start: 'title',
+              center: '',
+              end: 'today'
+            }}
+            footerToolbar={{
+              start: '',
+              center: '',
+              end: 'dayGridWeek dayGridMonth prev,next'
+            }}
+            events={
+              this.state.events.map( (event) => {
+                return ({
+                  title: event.name,
+                  start: event.startTime.getTime(),
+                  end: event.startTime.getTime()
+                })
+              })
+            }
+            eventClick={this.handleEventClick}
             initialView = 'dayGridMonth'
-	    dateClick= {this.handleDateClick}
-            height = 'auto'
-            width = 'auto'
+            fixedWeekCount={false}
+            dateClick= {this.handleDateClick}
+            height='100%'
           />
         </div>
-	<DateModal 
-	    show={this.state.dateModalVisible}
-	    onHide={this.hideDateModal}
-	    dateStr={this.state.dateClickedStr}
-	    events={this.state.dateClickedEvents}
-	></DateModal>
-      </Container>
+        <DateModal
+            show={this.state.showModal}
+            onHide={this.hideDateModal}
+            dateStr={this.state.dateClickedStr}
+            events={this.state.dateClickedEvents}
+            eventToggled={this.state.eventClicked}
+        />
+      </div>
     );
   }
-} 
+}
 
 export default CalendarPage;
--- frontend/src/DateModal.js
+++ frontend/src/DateModal.js
@@ -11,33 +11,33 @@ class DateModal extends React.Component {
     }
   }
 
-  getEventComponents(){
-    return (this.props.events.map((e) =>
-      <Collapsible trigger={e.name}>
-        {
-	  Object.keys(e).map(function(eventAttribute){
-            return <h6>{eventAttribute + ": " + e[eventAttribute]}</h6>
-          })
-	}
-        <div class="signUpBox"><Button class="signUpButton">Sign up</Button></div>
-      </Collapsible>));
+  getEventComponents = () => {
+    return (this.props.events.map((event) =>
+      <Collapsible open={this.props.eventToggled === event.name}  trigger={event.name}>
+          {Object.keys(event).map( (eventAttribute) => {
+                return <h6>{eventAttribute + ": " + event[eventAttribute]}</h6>
+          })}
+        <div className="signUpBox"><Button className="signUpButton">Sign up</Button></div>
+      </Collapsible>)
+    );
   }
 
   render() {
     return (
-      <>
-	<Modal show={this.props.show} onHide={this.props.onHide}>
+        <Modal centered show={this.props.show} onHide={this.props.onHide}>
           <Modal.Header>
             <Modal.Title>Events on {this.props.dateStr}</Modal.Title>
           </Modal.Header>
-          <Modal.Body>
-	    {this.getEventComponents()}
+          <Modal.Body style={{textAlign: "center"}}>
+            {this.props.events.length
+                ? this.getEventComponents()
+                : "No events. Check back soon!"
+            }
           </Modal.Body>
           <Modal.Footer>
             <Button onClick={this.props.onHide}>Close</Button>
           </Modal.Footer>
         </Modal>
-      </>
     );
   }
 }
