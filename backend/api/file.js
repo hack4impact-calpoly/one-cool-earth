54d0554190d57e953ef7363225a1b807061c1a19
sleepyStick <itsirisho@gmail.com>
2021-07-27 08:33:09 -0700

    fix: all endpoints in file.js should work now

--- backend/api/file.js
+++ backend/api/file.js
@@ -2,12 +2,24 @@ const express = require('express');
 const File = require('../models/File');
 const router = express.Router();
 const authEndpoint = require('./auth')
+const formidable = require('formidable');
+const fs = require('fs')
 
+router.post('/upload', authEndpoint.auth, async (req, res) => {
+   const form = formidable({ multiples: true });
 
-router.post('/upload', async (req, res) => {
-   // if (req.user.admin) {
-      console.log(req.body)
-      if (req.body.active) {
+   form.parse(req, async (err, fields, files) => {
+      if (err) {
+         next(err);
+         return;
+      }
+
+      if (req.user.admin) {
+      console.log(fields)
+      console.log(fields.fileName)
+      console.log(files)
+      console.log(files.File.path)
+      if (fields.active) {
          await File.updateMany(
             {},
             {$set: 
@@ -15,40 +27,41 @@ router.post('/upload', async (req, res) => {
             },
          )
       }
-      await File.findOne({"name": req.body.fileName}).then(function(file, err) {
+      await File.findOne({"name": fields.fileName}).then(function(file, err) {
          if (err)
-            res.sendStatus(400) //is that the right error?!?!??!
+            res.sendStatus(400) 
          if (!file) 
             file = new File();
-         file.name = req.body.fileName;
-         file.description = req.body.description;
-         file.active = req.body.active; //should it be false on upload??
-         file.file.data = req.body.fileData;
-         file.file.contentType = req.body.contentType
+
+         var newFile = fs.readFileSync(files.File.path)
+         var encFile = newFile.toString('base64')
+         var bin = new Buffer(encFile, 'base64')
+
+         file.name = fields.fileName;
+         file.description = fields.description;
+         file.active = false;
+         file.file.contentType = files.File.type;
+         file.file.data = bin;
          file.save()
       })
-      // const file = new File();
-      // file.name = req.body.fileName;
-      // file.description = req.body.description;
-      // file.active = req.body.active; //should it be false on upload??
-      // file.file.data = req.body.fileData;
-      // file.file.contentType = req.body.contentType
-      // file.save()
       res.sendStatus(200)
-   // } else {
-   //    res.sendStatus(403)
-   // }
+   } else {
+      res.sendStatus(403)
+   }
+   });
+   
 });
 
 router.delete('/delete', authEndpoint.auth, async(req, res) => {
    if(req.user.admin) {
+      console.log(req.body.fileName)
       fileName = req.body.fileName;
 
-      await File.findOneAndDelete(
-         {"name": fileName}
-      )
-
-      res.sendStatus(200)
+      await File.findOneAndDelete({"name": fileName}).then(function(err) {
+         if (err)
+            res.sendStatus(400)
+         res.sendStatus(200)
+      })
    } else {
       res.sensStatus(403)
    }
@@ -56,14 +69,14 @@ router.delete('/delete', authEndpoint.auth, async(req, res) => {
 
 router.post('/deactivate', authEndpoint.auth, async (req, res) => {
    if(req.user.admin) {
-      let fileName = req.body.fileName;
 
-      await File.findOne({"name": fileName}).then(function(file, err) {
-         if (err)
-            res.sendStatus(400) //is that the right error?!?!??!
-         file.active = false; 
-         file.save()
-      })
+      await File.updateMany(
+         {},
+         {$set: 
+            {active: false}
+         },
+      )
+
       res.sendStatus(200)
    } else {
       res.sendStatus(403)
@@ -72,6 +85,7 @@ router.post('/deactivate', authEndpoint.auth, async (req, res) => {
 
 router.post('/activate', authEndpoint.auth, async(req,res) => {
    console.log("calling activate")
+   console.log(req.body.fileName)
    if(req.user.admin) {
       let fileName = req.body.fileName;
 
@@ -86,7 +100,7 @@ router.post('/activate', authEndpoint.auth, async(req,res) => {
       // activate the one file
       await File.findOne({"name": fileName}).then(function(file, err) {
          if (err)
-            res.sendStates(400) //is that the right error?!?!??!
+            res.sendStatus(400)
          file.active = true; 
          file.save()
       })
@@ -95,6 +109,31 @@ router.post('/activate', authEndpoint.auth, async(req,res) => {
    } else {
       res.sendStatus(403)
    }
+})
+
+router.get('/get-all', authEndpoint.auth, async(req,res) => {
+   console.log(req.body)
+   console.log("calling get-all")
+   if (req.user.admin) {
+      await File.find({}).then(function(files, err) {
+         if (err)
+            res.sendStatus(400)
+         res.status(200)
+         res.send(files)
+      })
+   } else {
+      res.sendStatus(403)
+   }
+})
+
+router.get("/get", authEndpoint.auth, async(req, res) => {
+   console.log("calling get one")
+   await File.findOne({"active": true}).then(function(file, err) {
+      if (err)
+         res.sendStatus(400)
+      res.status(200)
+      res.send(file)
+   })
 })
 
 module.exports = router;
