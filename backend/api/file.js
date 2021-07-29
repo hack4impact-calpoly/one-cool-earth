120fef8610856945054eefcab310c3605604aac6
sleepyStick <itsirisho@gmail.com>
2021-07-29 08:15:42 -0700

    removed unused packages and print statements

--- backend/api/file.js
+++ backend/api/file.js
@@ -15,46 +15,41 @@ router.post('/upload', authEndpoint.auth, async (req, res) => {
       }
 
       if (req.user.admin) {
-      console.log(fields)
-      console.log(fields.fileName)
-      console.log(files)
-      console.log(files.File.path)
-      if (fields.active) {
-         await File.updateMany(
-            {},
-            {$set: 
-               {active: false}
-            },
-         )
+         if (fields.active) {
+            await File.updateMany(
+               {},
+               {$set: 
+                  {active: false}
+               },
+            )
+         }
+         await File.findOne({"name": fields.fileName}).then(function(file, err) {
+            if (err)
+               res.sendStatus(400) 
+            if (!file) 
+               file = new File();
+
+            var newFile = fs.readFileSync(files.File.path)
+            var encFile = newFile.toString('base64')
+            var bin = new Buffer(encFile, 'base64')
+
+            file.name = fields.fileName;
+            file.description = fields.description;
+            file.active = false;
+            file.file.contentType = files.File.type;
+            file.file.data = bin;
+            file.save()
+         })
+         res.sendStatus(200)
+      } else {
+         res.sendStatus(403)
       }
-      await File.findOne({"name": fields.fileName}).then(function(file, err) {
-         if (err)
-            res.sendStatus(400) 
-         if (!file) 
-            file = new File();
-
-         var newFile = fs.readFileSync(files.File.path)
-         var encFile = newFile.toString('base64')
-         var bin = new Buffer(encFile, 'base64')
-
-         file.name = fields.fileName;
-         file.description = fields.description;
-         file.active = false;
-         file.file.contentType = files.File.type;
-         file.file.data = bin;
-         file.save()
-      })
-      res.sendStatus(200)
-   } else {
-      res.sendStatus(403)
-   }
    });
    
 });
 
 router.delete('/delete', authEndpoint.auth, async(req, res) => {
    if(req.user.admin) {
-      console.log(req.body.fileName)
       fileName = req.body.fileName;
 
       await File.findOneAndDelete({"name": fileName}).then(function(err) {
@@ -84,8 +79,6 @@ router.post('/deactivate', authEndpoint.auth, async (req, res) => {
 });
 
 router.post('/activate', authEndpoint.auth, async(req,res) => {
-   console.log("calling activate")
-   console.log(req.body.fileName)
    if(req.user.admin) {
       let fileName = req.body.fileName;
 
@@ -112,8 +105,6 @@ router.post('/activate', authEndpoint.auth, async(req,res) => {
 })
 
 router.get('/get-all', authEndpoint.auth, async(req,res) => {
-   console.log(req.body)
-   console.log("calling get-all")
    if (req.user.admin) {
       await File.find({}).then(function(files, err) {
          if (err)
@@ -127,7 +118,6 @@ router.get('/get-all', authEndpoint.auth, async(req,res) => {
 })
 
 router.get("/get", authEndpoint.auth, async(req, res) => {
-   console.log("calling get one")
    await File.findOne({"active": true}).then(function(file, err) {
       if (err)
          res.sendStatus(400)
